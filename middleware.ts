4110b2ea312ec8cc0e85c6425f5632acb87340ed
warjamma <lgold141@gmail.com>
2026-01-08 17:13:36 +0700

    chore: debug

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
   const auth = req.headers.get("authorization");
-  
+  console.log('[DEBUG-TOKEN-AUTH]', auth);
 
   const requestHeaders = new Headers(req.headers);
   if (auth && !requestHeaders.has("authorization")) {
