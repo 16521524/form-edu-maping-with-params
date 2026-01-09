db0c15af5e14efed8d0ace597904107dea60d2e3
warjamma <lgold141@gmail.com>
2026-01-09 14:49:48 +0700

    fix: height table

--- components/mobile-table/MobileTable.tsx
+++ components/mobile-table/MobileTable.tsx
@@ -267,7 +267,7 @@ export function MobileTable({ leads = defaultLeads, loading, page = 1, pageSize 
     >
       <div
         ref={scrollRef}
-        className="overflow-x-auto overflow-y-auto scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-200 bg-white max-h-[45.5vh]"
+        className="overflow-x-auto overflow-y-auto scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-200 bg-white max-h-[80vh]"
         aria-busy={loading}
       >
         <div
